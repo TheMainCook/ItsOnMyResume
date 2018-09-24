4fb7a502e6492ff3c4849bc71030fe2fd133b8a7
michael cook <michaeldcookjr40@gmail.com>
2018-09-24 15:28:23 -0500

    Added copy buttons/ other functions

--- popup.js
+++ popup.js
@@ -5,22 +5,72 @@ let sbutton = document.getElementById("sbutton");
 let help = document.getElementById("help");
 
 
+const turnOn = () => {
+    let idb = document.querySelectorAll("button.idb");
+    idb.forEach(d => {
+       d.addEventListener("click",()=>{
+           console.log(d.innerHTML);
+           if ( document.getElementById(d.innerHTML).style.display == "none"){
+            document.getElementById(d.innerHTML).style.display = "block";
+           }
+           else{
+            document.getElementById(d.innerHTML).style.display = "none";
+           }
+       
+       });
+   });
+};
 
+const copyEle = () => {
+    let idb = document.querySelectorAll("img.copy");
+    idb.forEach(d => {
+       d.addEventListener("click",()=>{
+           console.log(d.alt);
+           
+            document.getElementById(d.alt).select();
+           document.execCommand('copy');
+       });
+   });
+};
 
 const reset = () => {
     chrome.storage.local.set({"Jobs":[]});
+    update();
 };
 
 const update = () => {
     chrome.storage.local.get("Jobs",function(result){
         let c = "";
-           console.log(result);
-        
-        
-        document.getElementById("AJcontent").innerText = c;
-        
+        console.log(result);
+        let num = 0;
+        result.Jobs != undefined? 
+result.Jobs.forEach(i =>{
+
+    c +=  '<br><button class="idb btn btn-primary btn-block">'+i[0]+'</button>'+
+    '<ul id ="'+i[0]+'" class="list-group" style="display: none" >'+
+    '<li class="list-group-item"><textarea class="crust" id="'+(num)+'" rows=1 cols=18 readonly>'+i[0]+'</textarea> <img src="copy.png" alt="'+(num)+'" class="copy" width="25" height="25" align="right"></li>'+
+    '<li class="list-group-item"><textarea class="crust" id="'+(num+1)+'" rows=1 cols=18 readonly>'+i[1]+'</textarea> <img src="copy.png" alt="'+(num+1)+'" class="copy" width="25" height="25" align="right"></li>'+
+    '<li class="list-group-item"><textarea class="crust" id="'+(num+2)+'" rows=1 cols=18 readonly>'+i[2]+'</textarea> <img src="copy.png" alt="'+(num+2)+'" class="copy" width="25" height="25" align="right"></li>'+
+    '<li class="list-group-item"><textarea class="crust" id="'+(num+3)+'" rows=1 cols=18 readonly>'+i[3]+'</textarea> <img src="copy.png" alt="'+(num+3)+'" class="copy" width="25" height="25" align="right"></li>'+
+    '<li class="list-group-item"><textarea class="crust" id="'+(num+4)+'" rows=1 cols=18 readonly>'+i[4]+'</textarea> <img src="copy.png" alt="'+(num+4)+'" class="copy" width="25" height="25" align="right"></li>'+
+    '</ul>'
+    num +=5; 
+})
+        : reset() ;
+
+        document.getElementById("AJcontent").innerHTML = c;
+        turnOn();
+        copyEle();
         });
+
 };
+
+
+
+
+
+
+
 
 update();
 
@@ -52,9 +102,10 @@ chrome.storage.local.get("Jobs",function(result){
     b.push(a);
 chrome.storage.local.set({"Jobs": b});
 console.log(result.Jobs);
+update();
 });
 
-update();
+
 form.style.display = "none";
 })
 
